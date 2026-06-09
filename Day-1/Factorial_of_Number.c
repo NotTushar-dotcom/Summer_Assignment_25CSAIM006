1f8192ac586d3f8cfa93d8c0f13d0a413f968640
NotTushar-dotcom <tushh45@gmail.com>
2026-06-09 13:53:50 +0530

    Factorial of a Number

--- Day-1/Factorial_of_Number.c
+++ Day-1/Factorial_of_Number.c
@@ -4,11 +4,15 @@
 
 int main(){
     int n, fact =1, i;
+
     printf("Enter the Number: ");
     scanf("%d", &n);
+
     for( i = 1; i <= n; i++){
         fact = fact * i;
     }
+
     printf("Factorial of a Number is: %d", fact);
+    
     return 0;
 }
