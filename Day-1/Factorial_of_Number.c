5e563dbed7e9e46ca74d07e97836cf9c49569f2c
NotTushar-dotcom <tushh45@gmail.com>
2026-06-09 13:51:24 +0530

    Factorial of a Number

---
+++ Day-1/Factorial_of_Number.c
@@ -0,0 +1,14 @@
+// Write a program to Find factorial of a number. 
+
+#include<stdio.h>
+
+int main(){
+    int n, fact =1, i;
+    printf("Enter the Number: ");
+    scanf("%d", &n);
+    for( i = 1; i <= n; i++){
+        fact = fact * i;
+    }
+    printf("Factorial of a Number is: %d", fact);
+    return 0;
+}
