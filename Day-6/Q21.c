4cf4c64bbc6a0b2867c049e7d64d80109e859b50
NotTushar-dotcom <tushh45@gmail.com>
2026-06-14 23:22:09 +0530

    Convert Decimal to Binary

--- Day-6/Q21.c
+++ Day-6/Q21.c
@@ -1,44 +1,24 @@
 // Write a program to Convert decimal to binary.
 
 #include <stdio.h>
-#include <string.h>
 
-int main() {
-    double num;
-    char int_bin[64] = ""; 
-    int i = 0;
+int main(void) {
+    int n, rem, binary = 0, base = 1;
 
-    printf("Enter a decimal number: ");
-    scanf("%lf", &num);
-
-    long long int_part = (long long)num;
-    double frac_part = num - int_part;
-
-    
-    if (int_part == 0) {
-        printf("0");
-    } else {
-        while (int_part > 0) {
-            int_bin[i++] = (int_part % 2) + '0'; 
-            int_part /= 2;
-        }
-        
-        for (int j = i - 1; j >= 0; j--) {
-            printf("%char", int_bin[j]); 
-        }
+    printf("Enter decimal number: ");
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input\n");
+        return 1;
     }
 
-    // 3. Process fractional part
-    if (frac_part > 0) {
-        printf(".");
-        for (int k = 0; k < 6; k++) { 
-            frac_part *= 2;
-            printf("%d", (int)frac_part);
-            frac_part -= (int)frac_part;
-        }
+    while (n > 0) {
+        rem = n % 2;
+        binary = binary + rem * base;
+        n = n / 2;
+        base = base * 10; // 1, 10, 100, ...
     }
-    
-    printf("\n");
+
+    printf("Binary = %d\n", binary);
     return 0;
 }
 
