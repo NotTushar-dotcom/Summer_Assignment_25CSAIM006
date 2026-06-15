e34bfbe14fb4b6201bf555aa35c786d14c43de3e
NotTushar-dotcom <tushh45@gmail.com>
2026-06-15 16:48:29 +0530

    Recursive factorial

---
+++ Da-7/Q25.c
@@ -0,0 +1,24 @@
+// Write a program to Recursive factorial. 
+
+#include <stdio.h>
+
+  int fact(int n){
+    if(n==0||n==1){
+        return 1;
+    } else {
+        return n*fact(n-1);
+    }
+  }
+
+  int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    if(num<0){
+     printf("Factorial does not exists");
+    }
+    else printf("Factorial of %d = %d\n", num, fact(num));
+
+    return 0;
+}
