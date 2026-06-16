afaa2cd741797173b5aea980f6726d81d10528a2
NotTushar.dotcom <tushh45@gmail.com>
2026-06-16 23:20:42 +0530

    Print Number Triangle

---
+++ Day-8/Q30.c
@@ -0,0 +1,24 @@
+// Write a program to Print number triangle. 
+
+// 1
+// 12 
+// 123 
+// 1234 
+// 12345 
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5; 
+
+   
+    for (i = 1; i <= rows; ++i) {
+        for (j = 1; j <= i; ++j) {
+            printf("%d", j);
+        }
+        printf("\n\n");
+    }
+
+    return 0;
+}
