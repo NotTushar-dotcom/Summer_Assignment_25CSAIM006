7a9d11886a77f81e9d8221e3d7a95fa889d00072
NotTushar.dotcom <tushh45@gmail.com>
2026-06-19 13:55:14 +0530

    Function to Find Sum of Two Numbers

---
+++ Day-11/Q41.c
@@ -0,0 +1,18 @@
+// Write a program to Write function to find sum of two numbers.
+
+#include<stdio.h>
+int add(int a, int b);
+
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    int result = add(x, y);
+    printf("Sum = %d\n", result);
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
