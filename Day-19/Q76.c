26a3825fddbc9928b915917ca5a504179c05c96e
NotTushar.dotcom <tushh45@gmail.com>
2026-06-28 19:30:54 +0530

    Find Diagonal Sum

---
+++ Day-19/Q76.c
@@ -0,0 +1,37 @@
+// Write a program to Find diagonal sum.
+
+#include <stdio.h>
+
+#define MAX 10
+
+int main() {
+	int n;
+	int matrix[MAX][MAX];
+	int diagonalSum = 0;
+
+	printf("Enter order of square matrix: ");
+	scanf("%d", &n);
+
+	if (n <= 0 || n > MAX) {
+		printf("Invalid matrix size.\n");
+		return 1;
+	}
+
+	printf("Enter elements of matrix:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		diagonalSum += matrix[i][i];
+		if (i != n - i - 1) {
+			diagonalSum += matrix[i][n - i - 1];
+		}
+	}
+
+	printf("Diagonal sum: %d\n", diagonalSum);
+
+	return 0;
+}
