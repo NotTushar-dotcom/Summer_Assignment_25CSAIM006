3bb788fc7bc11661014acb9d8dc8aac6aeebd452
NotTushar.dotcom <tushh45@gmail.com>
2026-06-28 19:30:16 +0530

    Add Matrices

---
+++ Day-19/Q73.c
@@ -0,0 +1,48 @@
+// Write a program to Add matrices.
+
+#include <stdio.h>
+
+#define MAX 10
+
+int main() {
+	int rows, cols;
+	int first[MAX][MAX], second[MAX][MAX], sum[MAX][MAX];
+
+	printf("Enter number of rows and columns: ");
+	scanf("%d %d", &rows, &cols);
+
+	if (rows <= 0 || cols <= 0 || rows > MAX || cols > MAX) {
+		printf("Invalid matrix size.\n");
+		return 1;
+	}
+
+	printf("Enter elements of first matrix:\n");
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			scanf("%d", &first[i][j]);
+		}
+	}
+
+	printf("Enter elements of second matrix:\n");
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			scanf("%d", &second[i][j]);
+		}
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			sum[i][j] = first[i][j] + second[i][j];
+		}
+	}
+
+	printf("Sum of matrices:\n");
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			printf("%d ", sum[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
