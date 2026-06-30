784d51e913e1f81f82f1b9c2ef514869448e6e26
NotTushar.dotcom <tushh45@gmail.com>
2026-06-30 12:34:37 +0530

    Check Palindrome String

---
+++ Day-22/Q85.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char str[100];
+	int left, right;
+	int isPalindrome = 1;
+
+	printf("Enter a string: ");
+	fgets(str, sizeof(str), stdin);
+
+	str[strcspn(str, "\n")] = '\0';
+
+	left = 0;
+	right = (int)strlen(str) - 1;
+
+	while (left < right) {
+		if (str[left] != str[right]) {
+			isPalindrome = 0;
+			break;
+		}
+		left++;
+		right--;
+	}
+
+	if (isPalindrome) {
+		printf("Palindrome string\n");
+	} else {
+		printf("Not a palindrome string\n");
+	}
+
+	return 0;
+}
